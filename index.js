c6f4f0290aa738bd69c3d34430ce5f8619ddf40c
Iyngcdeii <x.uyen050519@gmail.com>
2025-03-23 14:38:31 +0700

    Tích hợp PUG

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const path = require('path');
+
 const app = express()
 const port = 3000
 
+// Thiết lập views
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+
+
 app.get('/', (req, res) => {
-  res.send("Trang chủ 123")
+  res.render("client/pages/home", {
+    pageTitle: "Trang chủ"
+  })
 })
 
 app.get('/tours', (req, res) => {
-  res.send("Danh sách tour")
+  res.render("client/pages/tour-list", {
+    pageTitle: "Danh sách tour"
+  })
 })
 
 app.listen(port, () => {
