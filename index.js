64fa90f131d80f0d73605fad5e95bdef9ba73b8b
Iyngcdeii <x.uyen050519@gmail.com>
2025-03-23 15:04:59 +0700

    Nhúng file tĩnh của Frontend

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const port = 3000
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'pug');
 
+// Thiết lập thư mục chứa file tĩnh của Frontend
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get('/', (req, res) => {
   res.render("client/pages/home", {
