0624d25ab5edb10b1651341afa4eae2ac077a2e9
Iyngcdeii <x.uyen050519@gmail.com>
2025-03-26 12:19:02 +0700

    Tách Controller

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const path = require('path');
+require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://xuyen050519:FGc446Y2J9XReJHr@cluster0.22qm9.mongodb.net/tour-management');
+mongoose.connect(process.env.DATABASE);
 
-const Tour = require("./models/tour.model");
+const tourController = require("./controllers/client/tour.controller");
+const homeController = require("./controllers/client/home.controller");
 
 const app = express()
 const port = 3000
@@ -15,23 +17,9 @@ app.set('view engine', 'pug');
 // Thiết lập thư mục chứa file tĩnh của Frontend
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get('/', homeController.home)
 
-app.get('/', (req, res) => {
-  res.render("client/pages/home", {
-    pageTitle: "Trang chủ"
-  })
-})
-
-app.get('/tours', async (req, res) => {
-  const tourList = await Tour.find({});
-
-  console.log(tourList);
-
-  res.render("client/pages/tour-list", {
-    pageTitle: "Danh sách tour",
-    tourList: tourList
-  })
-})
+app.get('/tours', tourController.list)
 
 app.listen(port, () => {
   console.log(`Website đang chạy trên cổng ${port}`)
