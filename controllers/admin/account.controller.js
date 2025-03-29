66f130213ece7dd72fd1f8b5aeb16bd7f268aba5
Iyngcdeii <x.uyen050519@gmail.com>
2025-03-29 16:24:45 +0700

    Tích hợp giao diện trang Đăng ký

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -2,4 +2,10 @@ module.exports.login = async (req, res) => {
     res.render("admin/pages/login", {
       pageTitle: "Đăng nhập"
     })
-  }  
+  }
+
+  module.exports.register = async (req, res) => {
+    res.render("admin/pages/register", {
+      pageTitle: "Đăng ký"
+    })
+  }
