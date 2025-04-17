8ba49b62f4b4b1a7eb1e39ede4fabdca179b8e90
Iyngcdeii <x.uyen050519@gmail.com>
2025-04-17 22:09:50 +0700

    Hiển thị thông báo khi Tạo thành công

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -36,8 +36,9 @@ module.exports.list = async (req, res) => {
     const newRecord = new Category(req.body);
     await newRecord.save();
   
+    req.flash("success", "Tạo danh mục thành công!");
+    
     res.json({
-      code: "success",
-      message: "Tạo danh mục thành công!"
+      code: "success"
     })
   }  
