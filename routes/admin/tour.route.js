0352b70b38e3133d5036e1e3b096d2dc06cc7592
Iyngcdeii <x.uyen050519@gmail.com>
2025-04-25 10:01:56 +0700

    Làm trang Tạo tour

--- routes/admin/tour.route.js
+++ routes/admin/tour.route.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
+const multer  = require('multer');
+
+const cloudinaryHelper = require("../../helpers/cloudinary.helper");
 
 const tourController = require("../../controllers/admin/tour.controller");
+
+const tourValidate = require("../../validates/admin/tour.validate");
+
+const upload = multer({ storage: cloudinaryHelper.storage });
 
 router.get('/list', tourController.list)
 
 router.get('/create', tourController.create)
 
+router.post(
+    '/create', 
+    upload.single('avatar'), 
+    tourValidate.createPost,
+    tourController.createPost
+  )
+  
 router.get('/trash', tourController.trash)
 
 module.exports = router;
