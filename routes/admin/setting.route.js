1be1270e3dc0180d0c4d039a1a9a0f55e5f5e280
Iyngcdeii <x.uyen050519@gmail.com>
2025-05-03 15:22:39 +0700

    Làm trang Tạo nhóm quyền

--- routes/admin/setting.route.js
+++ routes/admin/setting.route.js
@@ -30,4 +30,6 @@ router.get('/role/list', settingController.roleList)
 
 router.get('/role/create', settingController.roleCreate)
 
+router.post('/role/create', settingController.roleCreatePost)
+
 module.exports = router;
