37569784589db701abc942464f04d3257fbd5cdd
Iyngcdeii <x.uyen050519@gmail.com>
2025-05-09 11:15:59 +0700

    Hiển thị data ra Section 2 (Trang chủ)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,26 @@
-module.exports.home = (req, res) => {
+const Tour = require("../../models/tour.model");
+const moment = require("moment");
+
+module.exports.home = async (req, res) => {
+  // Section 2
+  const tourListSection2 = await Tour
+    .find({
+      deleted: false,
+      status: "active"
+    })
+    .sort({
+      position: "desc"
+    })
+    .limit(6)
+
+  for(const item of tourListSection2) {
+    item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
+  }
+  // End Section 2
+
     res.render("client/pages/home", {
-      pageTitle: "Trang chủ"
+      pageTitle: "Trang chủ",
+      tourListSection2: tourListSection2  
     })
   }
   
