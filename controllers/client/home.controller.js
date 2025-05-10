869fa980e17c87d4088cd5e9c05005772194cf8a
Iyngcdeii <x.uyen050519@gmail.com>
2025-05-10 16:32:05 +0700

    Hiển thị data ra Section 4 (Trang chủ)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,6 @@
 const Tour = require("../../models/tour.model");
 const moment = require("moment");
+const categoryHelper = require("../../helpers/category.helper");
 
 module.exports.home = async (req, res) => {
   // Section 2
@@ -18,9 +19,30 @@ module.exports.home = async (req, res) => {
   }
   // End Section 2
 
+  // Section 4: Tour Trong Nước
+  const categoryIdSection4 = "67ff5cdabe22376ce45c4d2a"; // id danh mục Tour Trong Nước
+  const listCategoryId = await categoryHelper.getAllSubcategoryIds(categoryIdSection4);
+
+  const tourListSection4 = await Tour
+    .find({
+      category: { $in: listCategoryId },
+      deleted: false,
+      status: "active"
+    })
+    .sort({
+      position: "desc"
+    })
+    .limit(8)
+
+  for(const item of tourListSection4) {
+    item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
+  }
+  // End Section 4: Tour Trong Nước
+
     res.render("client/pages/home", {
       pageTitle: "Trang chủ",
-      tourListSection2: tourListSection2  
+      tourListSection2: tourListSection2,
+      tourListSection4: tourListSection4
     })
   }
   
